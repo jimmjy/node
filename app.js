a75bc69181caa87ff109c202202aef9ecb17da8b
James Finkelstein <jamesfinkelstein@Jamess-MacBook.local>
2019-01-12 17:23:00 -0500

    added routing

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-	console.log('product page');
-	res.send('<h1>the "add-product" page</h1>');
-});
+const adminRoutes = require('./routes/admin');
 
-app.use('/', (req, res, next) => {
-	console.log('In another middleware');
-	res.send('<h1>Hello from express again!</h1>');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
