a4c6151e626e7a3d1bdae4cd52ebb1b7b22aacde
James Finkelstein <jamesfinkelstein@Jamess-MacBook.local>
2019-01-12 18:41:03 -0500

    added html views

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+// this is reached at /admin/add-product because we added '/admin' to route => get
 router.get('/add-product', (req, res, next) => {
-	res.send(
-		'<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>',
-	);
+	res.send();
+	console.log('hell');
 });
 
-router.post('/product', (req, res, next) => {
+// this is reached at /admin/add-product because we added '/admin' to route => post
+router.post('/add-product', (req, res, next) => {
 	console.log(req.body.title);
 	res.redirect('/');
 });
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const path = require( 'path' );
+const express = require( 'express' );
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-	res.send('<h1>Hello from express again!</h1>');
-});
+router.get( '/', ( req, res, next ) => {
+	res.sendFile( path.join( __dirname, '../views/shop.html' ) );
+} );
 
 module.exports = router;
